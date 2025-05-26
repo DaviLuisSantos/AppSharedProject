6d3985f82905447e9d255fed92002f40f91f39c4
Lucas <lucaselorenzohenrique@gmail.com>
2025-05-26 10:47:26 -0300

    relacionando morador e usuario

--- Models/Sqlite/Morador.cs
+++ Models/Sqlite/Morador.cs
@@ -22,6 +22,8 @@ public partial class Morador : BaseEntity
     public string? Celular { get; set; }
     public bool Admin { get; set; }
     public StatusMorador Status { get; set; }
+    [MemoryPackIgnore]
+    public User User { get; set; }
 
     [MemoryPackConstructor]
     public Morador() { }
---
+++ Models/Sqlite/User.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using AppSharedProject.DTO;
+using MemoryPack;
+
+namespace AppSharedProject.Models.Sqlite;
+
+[MemoryPackable]
+public partial class User : BaseEntity
+{
+
+    [Key]
+    public Guid Id { get; set; }
+    [Required]
+    public string Username { get; set; }
+    [Required]
+    public string Password { get; set; }
+    public Guid MoradorId { get; set; }
+    public Morador Morador { get; set; }
+
+    [MemoryPackConstructor]
+    public User() { }
+
+}
