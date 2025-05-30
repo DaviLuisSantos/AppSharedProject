b10c9d23c74eab1b8162de122c26ef23b017bf4b
Davi Luis <costasantos.daviluis@gmail.com>
2025-05-30 16:04:26 -0300

    Add `ControlIdUsuario` and `Veiculo` classes
    Introduced `ControlIdUsuario` class in `AppSharedProject.Models` to represent user entities with control ID attributes. Added `Veiculo` class in `AppSharedProject.Models.Firebird` for vehicle-related data, including database mappings and computed properties. Updated `AppSharedProject.projitems` to include `ControlIdUsuario`. Exposed `VEICULO_DENTRO` column as `VeiculoDentroInt` with a computed boolean property for usability.

---
+++ Models/ControlIdUsuario.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppSharedProject.Models;
+
+public class ControlIdUsuario
+{
+    public int Id { get; set; }
+    public string Nome { get; set; }
+    public int Matricula { get; set; }
+    public byte[] Foto { get; set; }
+    public int Grupo { get; set; } = 1;
+    public DateTime ValidadeInicio { get; set; } = DateTime.MinValue;
+    public DateTime ValidadeFim { get; set; } = DateTime.MaxValue;
+}
