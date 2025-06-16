3163c87042efe5ad441aa6b43e503213f1fe6fb6
Lucas <lucaselorenzohenrique@gmail.com>
2025-06-16 11:56:56 -0300

    inserindo campo IdFirebird em model visitantes

--- DTOs/MoradorFiltradoPorLiberacaoDTO.cs
+++ DTOs/MoradorFiltradoPorLiberacaoDTO.cs
@@ -13,30 +13,48 @@ namespace AppSharedProject.DTO
 
         public string? Unidade { get; set; }
 
+        public Unidade UnidadeNavigation { get; set; }
+
         public string? Celular { get; set; }
 
         public string? Cpf { get; set; }
 
         public string? Email { get; set; }
 
-        public bool Liberado { get; set; }
+        public bool Admin { get; set; }
 
         public DateTime? DataHoraCadastro { get; set; }
 
         public DateTime? DataHoraAlteracao { get; set; }
 
-        public MoradorFiltradoPorLiberacaoDTO(Morador morador, bool liberado)
+        public DateOnly? ValidadeDataInicial { get; set; }
+
+        public TimeOnly? ValidadeHoraInicial { get; set; }
+
+        public DateOnly? ValidadeDatFinal { get; set; }
+
+        public TimeOnly? ValidadeHoraFinal { get; set; }
+
+        public int? AutoArquivamento { get; set; }
+
+        public MoradorFiltradoPorLiberacaoDTO(Morador morador, bool admin)
         {
             Id = morador.Id;
             Nome = morador.Nome;
             Foto = morador.Foto;
             Unidade = morador.Unidade;
+            UnidadeNavigation = morador.UnidadeNavigation;
             Celular = morador.Celular;
             Cpf = morador.Cpf;
             Email = morador.Email;
-            Liberado = liberado;
+            Admin = admin;
             DataHoraCadastro = morador.DataHoraCadastro;
             DataHoraAlteracao = morador.DataHoraAlteracao;
+            ValidadeDataInicial = morador.ValidadeDataInicial;
+            ValidadeHoraInicial = morador.ValidadeHoraInicial;
+            ValidadeDatFinal = morador.ValidadeDatFinal;
+            ValidadeHoraFinal = morador.ValidadeHoraFinal;
+            AutoArquivamento = morador.AutoArquivamento;
         }
     }
 }
--- Models/Sqlite/Visitante.cs
+++ Models/Sqlite/Visitante.cs
@@ -6,16 +6,27 @@ namespace AppSharedProject.Models.Sqlite;
 public class Visitante : BaseEntity
 {
     [Key]
-    public Guid Id { get; set; }
+    public Guid Id { get; set; } = new Guid();
+    public int? IdFirebird { get; set; }
     public string Nome { get; set; }
     public string? Celular { get; set; }
     public string? Cpf { get; set; }
     public byte[]? Foto { get; set; }
+    public string? Documento { get; set; }
+    public string? Empresa { get; set; }
+    public string? Telefone { get; set; }
+    public string? Email { get; set; }
+    public string? Placa { get; set; }
+    public string? CarroCor { get; set; }
+    public string? CarroMarca { get; set; }
+    public string? Observacao { get; set; }
+    public DateTime? DataInicio { get; set; }
+    public DateTime? DataFim { get; set; }
+
     [ForeignKey("Morador")]
-    public Guid MoradorId { get; set; }
+    public Guid? IdMorador { get; set; }
     public Morador Morador { get; set; }
     public StatusVisitante Status { get; set; }
-
     public enum StatusVisitante
     {
         Cliente,
